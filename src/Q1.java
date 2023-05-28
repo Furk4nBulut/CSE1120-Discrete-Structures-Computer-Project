bf652dd6cd3474b5e2915bc7f9c576d55c1cd278
Furkan Bulut <furkanbtng@gmail.com>
2023-05-28 21:15:11 +0300

    Q1 B completed

--- src/Q1.java
+++ src/Q1.java
@@ -1,2 +1,15 @@
 public class Q1 {
+    public static void main(String[] args) {
+        Q1 q1 = new Q1();
+        System.out.println(q1.validation(3));
+    }
+    public int validation(int input) {
+        //1 + 2 + 2^2+2^3 + .... + 2^n = 2^(n+1) - 1
+        int sum = 0;
+        for (int i = 0; i <= input; i++) {
+            sum += Math.pow(2, i);
+        }
+        return sum;
+
+    }
 }
